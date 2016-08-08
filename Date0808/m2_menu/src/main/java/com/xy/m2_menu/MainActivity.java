02d9bbe1c1d7eebf161500ac2a1d3b65a7354ec3
knightxy <mortimerjava@gmail.com>
2016-08-08 16:06:37 +0800

    Menu
    普通的Topbar菜单和上下文菜单示例

--- Date0808/m2_menu/src/main/java/com/xy/m2_menu/MainActivity.java
+++ Date0808/m2_menu/src/main/java/com/xy/m2_menu/MainActivity.java
@@ -2,12 +2,60 @@ package com.xy.m2_menu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView mtextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mtextView = (TextView) findViewById(R.id.text);
+        //对控件设置上下文菜单，当长按这个控件后，弹出菜单选项
+        registerForContextMenu(mtextView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_item,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.apple:
+                Toast.makeText(MainActivity.this, "apple", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.banana:
+                Toast.makeText(MainActivity.this, "banana", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.littleIce:
+                Toast.makeText(MainActivity.this, "littleIce", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.plusIce:
+                Toast.makeText(MainActivity.this, "plusIce", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_ctx_item,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        return super.onContextItemSelected(item);
     }
 }
